4991c2d1a211b27825d4156726a024ff5cfb2525
Donlle Guo <guodongrui@outlook.com>
2017-05-23 07:42:54 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import org.eclipse.swt.widgets.Display;
 import org.lbchild.window.MainWindow;
-import org.lbchild.window.TestWindow;
 
 public class Main {
     public static void main(String[] args) {
